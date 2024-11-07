0f218e6bfad00bb3ffbfdc6d1000632c94ca5a40
Nexusdigitalia <nexusdigitaliasocialmedia@gmail.com>
2024-11-07 19:22:56 +0530

    home vercel update

---
+++ src/Seperate/Home2.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+// import "../Assect/css/new2.css"
+
+// function FullScreenImage() {
+//   return (
+//     <div className="fullscreen-image">
+//       <img
+//         src={require("../Assect/image/scam_web.jpeg")}
+//         alt=""
+//         className="img-fluid d-none d-md-block web-image"
+//       />
+//       <img
+//         src={require("../Assect/image/scam_mobile.jpeg")}
+//         alt=""
+//         className="img-fluid d-md-none mobile-image"
+//       />
+//     </div>
+//   );
+// }
+
+// export default FullScreenImage;
